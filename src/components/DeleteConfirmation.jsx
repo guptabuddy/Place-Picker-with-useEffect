1604ae73ff39d64622214495d8863f8cb1d88348
guptabuddy <guptabudy@gmail.com>
2025-11-04 18:29:36 +0530

    Remove Bug using useEffect, that Bug cause the place to be removed even when we click No on Dialog OR we pless Esc key. Basically to stop Timer

--- src/components/DeleteConfirmation.jsx
+++ src/components/DeleteConfirmation.jsx
@@ -1,7 +1,17 @@
+import { useEffect } from "react";
+
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
-	setTimeout(() => {
-		onConfirm();
-	}, 3000);
+	useEffect(() => {
+		console.log("Timer set");
+		const timerId = setTimeout(() => {
+			onConfirm();
+		}, 3000);
+
+		return () => {
+			console.log("Timer cleared");
+			clearTimeout(timerId);
+		};
+	}, []);
 
 	return (
 		<div id="delete-confirmation">
